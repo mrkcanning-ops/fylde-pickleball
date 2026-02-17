06c4096ad518103db6cdb9141098972dfa6c1beb
Mark <mrkcanning@yahoo.co.uk>
2026-02-17 12:14:43 +0000

    Initial commit - Fylde Pickleball homepage

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>🏓 Fylde Pickleball</h1>
+      <p>Welcome to your competitive ladder system.</p>
+      <p>Next step: we’ll connect this to a real database.</p>
+    </main>
+  );
+}
