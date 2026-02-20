78ab2088bac7695058930927fc6e9f4cf25375c2
Mark <mrkcanning@yahoo.co.uk>
2026-02-20 13:13:19 +0000

    Add full tab pages and components

---
+++ components/AddPlayerButton.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function AddPlayerButton() {
+  return (
+    <button className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+      Add Player
+    </button>
+  );
+}
