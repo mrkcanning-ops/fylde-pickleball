c49ad34b10d3168861af017f3c9ff49ac10dc063
Mark <mrkcanning@yahoo.co.uk>
2026-02-20 23:47:13 +0000

    Fix previous-matches prerender error

--- app/previous-matches/page.js
+++ app/previous-matches/page.js
@@ -1,5 +1,7 @@
 import PreviousMatchesClient from "./PreviousMatchesClient";
 
+export const dynamic = "force-dynamic";
+
 export default function PreviousMatchesPage() {
   return <PreviousMatchesClient />;
 }
